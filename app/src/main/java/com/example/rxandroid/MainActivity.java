9fd9adc84ca4433ebd0a15fe71be9c237c9207ba
Abdul Samad <abdulsamad@TetraNoodle.local>
2019-05-07 14:39:47 +0300

    Added RX dependicies. Created a basic Hello Java Rx program.

--- app/src/main/java/com/example/rxandroid/MainActivity.java
+++ app/src/main/java/com/example/rxandroid/MainActivity.java
@@ -1,13 +1,54 @@
 package com.example.rxandroid;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private static String TAG = "RX-Java";
+    private String rxJava = "Hello fro RX Java";
+    Observable<String> observable;
+    Observer<String> observer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        observable = Observable.just(rxJava);
+        observer = new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d(TAG, "Disposed");
+            }
+
+            @Override
+            public void onNext(String s) {
+
+                Log.d(TAG, s);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+                Log.d(TAG, e.getLocalizedMessage());
+            }
+
+            @Override
+            public void onComplete() {
+
+                Log.d(TAG, "Completed");
+            }
+        };
+        observable.subscribe(observer);
+
+
     }
 }
