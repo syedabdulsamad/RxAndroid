33487d30cd2ff781e9853fd4ebf19556b85dd710
Abdul Samad <abdulsamad@TetraNoodle.local>
2019-05-16 15:19:12 +0300

    Subjects example.

--- app/src/main/java/com/example/rxandroid/MainActivity.java
+++ app/src/main/java/com/example/rxandroid/MainActivity.java
@@ -3,19 +3,14 @@ package com.example.rxandroid;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.TextView;
 
 import com.example.rxandroid.Model.Student;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import io.reactivex.Observable;
-import io.reactivex.ObservableEmitter;
-import io.reactivex.ObservableSource;
-import io.reactivex.functions.Function;
-import io.reactivex.functions.Predicate;
 import io.reactivex.observers.DisposableObserver;
+import io.reactivex.subjects.AsyncSubject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
     private String[] rxJava = {"Hello", "from", "RX", "Java"};
     Observable<Student> observable;
     DisposableObserver<Student> disposableObserver;
+
+    // Example of AsyncSubject
+    AsyncSubject<Student> asyncSubject;
 
 
     @Override
@@ -37,24 +35,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<Student>students = new ArrayList<Student>();
-        students.add(new Student("Syed","address1", 20));
-        students.add(new Student("Syed","address1", 20));
-        students.add(new Student("Abdul","address2", 22));
-        students.add(new Student("Abdul","address2", 22));
-        students.add(new Student("Samad","address3", 234));
-        students.add(new Student("Samad","address3", 254));
+        ArrayList<Student> students = new ArrayList<Student>();
+        students.add(new Student("Syed", "address1", 20));
+        students.add(new Student("Syed", "address1", 20));
+        students.add(new Student("Abdul", "address2", 22));
+        students.add(new Student("Abdul", "address2", 22));
+        students.add(new Student("Samad", "address3", 234));
+        students.add(new Student("Samad", "address3", 254));
 
-        observable = Observable.fromIterable(students).distinct(s -> s.getAge());
 
+        observable = Observable.fromIterable(students).distinct(s -> s.getAge())
+                .skip(1) // skip first n operators
+                .skipLast(1); // skip last n operators
+
+        // creating the subject
+        asyncSubject = AsyncSubject.create();
+        // observable subscribing with Subject
+        observable.subscribe(asyncSubject);
 
         disposableObserver = new DisposableObserver<Student>() {
             @Override
             public void onNext(Student s) {
                 //  textView.setText(s);
-
-                Log.d(TAG, ""+s.getName());
-
+                Log.d(TAG, "" + s.getName());
             }
 
             @Override
@@ -67,6 +70,9 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(TAG, "Completed");
             }
         };
-        observable.subscribeWith(disposableObserver);
+        //observable.subscribeWith(disposableObserver);
+
+        // Async subject subscribing with Observer.
+        asyncSubject.subscribe(disposableObserver);
     }
 }
